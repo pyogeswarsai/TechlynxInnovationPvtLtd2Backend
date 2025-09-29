d893b150867b680e4615ffa878d757085df7a675
pyogeswarsai <pyogeswarsai1249@gmail.com>
2025-09-29 16:46:53 +0530

    changed in app js file

--- src/app.js
+++ src/app.js
@@ -20,7 +20,15 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://techlynxcoachingwebsite.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use("/uploads", express.static(process.env.UPLOAD_DIR || "uploads"));
 
