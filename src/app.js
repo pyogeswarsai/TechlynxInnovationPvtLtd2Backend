c9b76c6bfaa2f489126583956c9ef0885f913ef2
pyogeswarsai <pyogeswarsai1249@gmail.com>
2025-09-29 16:36:16 +0530

    changes like cors

--- src/app.js
+++ src/app.js
@@ -20,7 +20,18 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
-app.use(cors());
+const corsOptions = {
+  origin: 'https://techlynxcoachingwebsite.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions));
+
+
 app.use(express.json());
 app.use("/uploads", express.static(process.env.UPLOAD_DIR || "uploads"));
 
